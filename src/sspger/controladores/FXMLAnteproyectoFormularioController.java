a3388c51a92d4da0ca9e7adef1a5e0fe78392697
Cesar <50472267+EmilCes@users.noreply.github.com>
2023-06-02 17:57:43 -0600

    Formulario Anteproyecto

--- src/sspger/controladores/FXMLAnteproyectoFormularioController.java
+++ src/sspger/controladores/FXMLAnteproyectoFormularioController.java
@@ -2,14 +2,23 @@ package sspger.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 
 
 
 public class FXMLAnteproyectoFormularioController implements Initializable {
 
+    @FXML
+    private AnchorPane apCrearAnteproyecto;
+    @FXML
+    private ScrollPane spAnteproyectoFormulario;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        spAnteproyectoFormulario.setFocusTraversable(false);
     }    
     
 }
