394207c2b06a55e39656a3ad4acdc99dcfe3eb60
CameliaK <kirbithalberto@gmail.com>
2023-06-02 17:53:41 -0600

    css
    sc

--- src/sspger/controladores/FXMLRegistrarUsuarioController.java
+++ src/sspger/controladores/FXMLRegistrarUsuarioController.java
@@ -3,8 +3,12 @@ package sspger.controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -16,6 +20,24 @@ public class FXMLRegistrarUsuarioController implements Initializable {
 
     @FXML
     private AnchorPane apCrearAnteproyecto;
+    @FXML
+    private ComboBox<?> cbTipoUsuario;
+    @FXML
+    private TextField tfNombre;
+    @FXML
+    private TextField tfApellidoPaterno;
+    @FXML
+    private TextField tfApellidoMaterno;
+    @FXML
+    private TextField tfCorreo;
+    @FXML
+    private TextField tfNumeroTelefonico;
+    @FXML
+    private Button btnSeleccionarFoto;
+    @FXML
+    private Button btnGuardarUsuario;
+    @FXML
+    private TextField tfContraseña;
 
     /**
      * Initializes the controller class.
@@ -24,5 +46,13 @@ public class FXMLRegistrarUsuarioController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clicBtnSeleccionarFoto(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicBtnGuardarUsuario(ActionEvent event) {
+    }
     
 }
